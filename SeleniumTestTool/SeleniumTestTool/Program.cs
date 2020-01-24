240da7cd55dfc7e5c369baeb4f0a546703d70af7
yagmurD <myagmurdereli@hotmail.com>
2020-01-24 17:48:21 +0300

    Update Program.cs

--- SeleniumTestTool/SeleniumTestTool/Program.cs
+++ SeleniumTestTool/SeleniumTestTool/Program.cs
@@ -12,14 +12,13 @@ namespace SeleniumTestTool
     {
         static void Main(string[] args)
         {
-            //Sitenin açılıp açılmadığını testi
             IWebDriver driver = new FirefoxDriver(); 
             string link = @"https://www.n11.com/";
             driver.Navigate().GoToUrl(link);
 
             ////Login Test
             driver.FindElement(By.ClassName("btnSignIn")).Click();
-            driver.FindElement(By.Id("email")).SendKeys("yagmurereli@hotmail.com");
+            driver.FindElement(By.Id("email")).SendKeys("qweweqr@gmail.com");
             driver.FindElement(By.Id("password")).SendKeys("sadsasd");
             driver.FindElement(By.Id("loginButton")).Click();
 
